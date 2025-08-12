ae940c30005176e5cc29bf61c7ff9836ff329746
Prabhat Mishra <workforprabhat1254@gmail.com>
2025-08-12 18:58:50 +0530

    Edit the site metadata.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-    title: "Prabhat",
-    description: "Hello, I'm Prabhat, a software developer.",
+    title: "Prabhat | Software Developer",
+    description:
+        "I'm a self-taught software developer, buildling software the way some people build puzzles, piece by piece, until everything clicks.",
+    openGraph: {
+        title: "Prabhat | Software Developer",
+        description:
+            "I'm a self-taught software developer, buildling software the way some people build puzzles, piece by piece, until everything clicks.",
+        url: "https://prabhatmishra.vercel.app/",
+        siteName: "Prabhat Mishra",
+        images: [
+            {
+                url: "https://prabhatmishra.vercel.app/preview.png",
+                width: 1200,
+                height: 630,
+                alt: "Preview",
+            },
+        ],
+        locale: "en_US",
+        type: "website",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "Prabhat | Software Developer",
+        description:
+            "I'm a self-taught software developer, buildling software the way some people build puzzles, piece by piece, until everything clicks.",
+        images: ["https://prabhatmishra.vercel.app/preview.png"],
+        creator: "@prabhatsuntoh",
+    },
 };
 
 export default function RootLayout({
