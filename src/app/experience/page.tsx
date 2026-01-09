2615fdd68075ea40af0b903b75e9b43985935e98
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-01-10 01:00:04 +0530

    Some chnages

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -1,3 +1,10 @@
 export default function Experiences() {
-    return <div>Working....</div>;
+    return (
+        <div className="relative max-w-6xl mx-auto min-h-dvh bg-[#0073D1] flex items-center justify-center">
+            <div className="text-start flex flex-col gap-4 w-full px-6 md:px-8 xl:px-20 ">
+                <h1 className="text-4xl font-bold">Under Construction</h1>
+                <p className="text-xl">This page is currently being built.</p>
+            </div>
+        </div>
+    );
 }
