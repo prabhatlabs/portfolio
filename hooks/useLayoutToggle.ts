b0e2f34b22f262e8728723f083a26fc273c23925
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-03-10 01:10:08 +0530

    Pattern done

--- hooks/useLayoutToggle.ts
+++
@@ -1,9 +0,0 @@
-import { layoutContext, LayoutContextType } from "@/contexts/LayoutContext";
-import { useContext } from "react";
-
-export default function useLayoutToggle() {
-    const ctx = useContext<LayoutContextType | null>(layoutContext);
-    return {
-        ...ctx
-    }
-}
