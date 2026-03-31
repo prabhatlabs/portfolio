f2e1f6508b26a6d5b07e2d9ea70ff0940902282e
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-03-31 13:18:55 +0530

    exprience sesion done

--- app/_components/experiance.tsx
+++ app/_components/experiance.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 
 export function Experiance() {
     return (
-        <div className="space-y-4">
-            <h2 className="p-6 mt-6 sm:mt-8 md:mt-10 border-y text-3xl md:text-5xl font-bold">
+        <div className="">
+            <h2 className="p-6 mt-6 sm:mt-8 md:mt-10 border-t text-3xl md:text-5xl font-bold">
                 <LineShadowText
                     className="italic"
                     shadowColor={"var(--foreground)"}
@@ -14,19 +14,21 @@ export function Experiance() {
                 </LineShadowText>
             </h2>
 
-            <div className="px-6 space-y-4 border-b pb-4">
-                {experiences.map((experience) => (
+            <div className="">
+                {experiences.map((experience, index) => (
                     <div
-                        key={experience.company}
-                        className="flex flex-col sm:flex-row sm:gap-2 justify-between sm:items-end"
+                        key={index}
+                        className={`flex sm:gap-2 justify-between px-4 ${index !== 0 ? "border-b" : "border-y"}`}
                     >
-                        <div className="flex items-center gap-2 sm:gap-4">
+                        <div
+                            className={`flex items-center gap-2 sm:gap-4 w-full sm:w-fit px-2 py-4 border-l border-r sm:border-r-0`}
+                        >
                             <Image
                                 src={experience.iconUrl}
                                 alt={experience.company}
                                 width={50}
                                 height={50}
-                                className="size-10 sm:size-12 rounded"
+                                className="size-10 sm:size-12 rounded border"
                             />
                             <div className="">
                                 <h3 className="sm:text-lg font-semibold">
@@ -36,11 +38,16 @@ export function Experiance() {
                                     {experience.position} •{" "}
                                     <i>{experience.location}</i>
                                 </p>
+                                <p className="block sm:hidden text-sm text-muted-foreground">
+                                    {experience.period}
+                                </p>
                             </div>
                         </div>
-                        <p className="ml-12 sm:ml-0 text-sm text-muted-foreground">
-                            {experience.period}
-                        </p>
+                        <div className="hidden sm:flex items-end justify-end border-x w-42 px-2 py-4">
+                            <p className="text-sm text-muted-foreground">
+                                {experience.period}
+                            </p>
+                        </div>
                     </div>
                 ))}
             </div>
